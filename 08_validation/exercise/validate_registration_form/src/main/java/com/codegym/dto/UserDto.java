6dcb0a2a6fa5bba98ac0dd5bff814a3099619aaf
TechCare <you@example.com>
2021-08-31 17:26:18 +0700

    commit

--- 08_validation/exercise/validate_registration_form/src/main/java/com/codegym/dto/UserDto.java
+++ 08_validation/exercise/validate_registration_form/src/main/java/com/codegym/dto/UserDto.java
@@ -1,11 +1,17 @@
 package com.codegym.dto;
 
 import com.codegym.model.entity.User;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
 import javax.validation.constraints.*;
 
+@Setter
+@Getter
+@NoArgsConstructor
 public class UserDto implements Validator {
     private Integer id;
 
@@ -25,57 +31,6 @@ public class UserDto implements Validator {
 
     @Email
     private String email;
-
-    public UserDto() {
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 
     @Override
     public boolean supports(Class<?> clazz) {
--- 08_validation/exercise/validate_registration_form/src/main/java/com/codegym/model/entity/User.java
+++ 08_validation/exercise/validate_registration_form/src/main/java/com/codegym/model/entity/User.java
@@ -1,76 +1,29 @@
 package com.codegym.model.entity;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.validation.constraints.Email;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.Size;
+
 
 @Entity
+@Setter
+@Getter
+@NoArgsConstructor
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-
-    @NotEmpty
-    @Size(min = 5, max = 45)
     private String firstName;
-
-    @NotEmpty
-    @Size(min = 5, max = 45)
     private String lastName;
-
     private String phoneNumber;
-
-    @Min(18)
     private int age;
-
-    @Email
     private String email;
 
-    public User() {
-    }
 
-    public String getFirstName() {
-        return firstName;
-    }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
