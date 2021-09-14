22800bb8c04b55a2c876b438199455ba2db03286
TechCare <you@example.com>
2021-09-14 16:59:40 +0700

    commit

---
+++ 14_spring_security/practice/use_spring_security/src/main/java/com/codegym/model/repository/AppUerRepository.java
@@ -0,0 +1,10 @@
+package com.codegym.model.repository;
+
+import com.codegym.model.entity.AppUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AppUerRepository extends JpaRepository<AppUser, Long> {
+    AppUser findByUserName(String userName);
+}
